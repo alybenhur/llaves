93d261746f60eab47ce4afe439d9d55e7fa68858
aly <alybenhur@gmail.com>
2025-04-07 07:52:38 -0500

    modificado puerto

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const port = process.env.PORT || 8001; // Usa 8001 si no hay variable de entorno
+  const port = 10000; // Usa 8001 si no hay variable de entorno
    await app.listen(port, '0.0.0.0'); // 
   //await app.listen(8001);
 }
