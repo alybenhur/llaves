662d2c7f24eb9e3846905a34686c896b67191ea0
aly <alybenhur@gmail.com>
2025-04-06 19:51:43 -0500

    me doy

--- src/api/index.js
+++
@@ -1,5 +0,0 @@
-// api/index.js
-import { createApp } from '@nestjs/core';
-import { AppModule } from '../dist/app.module';
-
-export default createApp(AppModule).getHttpAdapter().getInstance();
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8001);
+  const port = process.env.PORT || 8001; // Usa 8001 si no hay variable de entorno
+   await app.listen(port, '0.0.0.0'); // 
+  //await app.listen(8001);
 }
 bootstrap();
